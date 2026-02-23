8152373dda272d1338cfbec6c04978ba257ff628
sanntiago.rvm <rvso240490@upemor.edu.mx>
2026-02-23 13:06:17 -0600

    hola 2

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -7,7 +7,9 @@ export default [
     config: {
       origin: [
         "http://localhost:3000",
-        "https://apiredsocialbk-production.up.railway.app"
+        "https://apiredsocialbk-production.up.railway.app",
+        "https://api-red-social-sannthiago-dev666s-projects.vercel.app",
+        "https://api-red-social-mu.vercel.app"
       ],
       methods: ["GET", "POST", "PUT", "DELETE"],
       headers: ["Content-Type", "Authorization"],
